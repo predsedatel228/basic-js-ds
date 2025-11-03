37d70a88884c28f4662fe127242dd0d645b88b91
Egor <predsedatel228@yandex.ru>
2025-11-03 19:33:52 +0300

    fix: fix solution for binary search tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../lib/errors');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
